5270efb7a1189ef4a0213bbfddc3024164077867
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-18 13:33:39 +0200

    Fix appUrl fallback

--- app/shopify.server.ts
+++ app/shopify.server.ts
@@ -12,7 +12,7 @@ const shopify = shopifyApp({
   apiSecretKey: process.env.SHOPIFY_API_SECRET || "",
   apiVersion: ApiVersion.October25,
   scopes: process.env.SCOPES?.split(","),
-  appUrl: process.env.APP_URL || "",
+  appUrl: process.env.APP_URL || "https://dpd-shopify-oken.vercel.app",
   authPathPrefix: "/auth",
   sessionStorage: new PrismaSessionStorage(prisma),
   distribution: AppDistribution.AppStore,
