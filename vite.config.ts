0c1b441d53b92bb3f0ce1c029a928328fe0fda9e
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-18 15:21:24 +0200

    Disable auto URL update

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // so we can remove this workaround after the next major release.
 if (
   process.env.HOST &&
-  (!process.env.SHOPIFY_APP_URL ||
-    process.env.SHOPIFY_APP_URL === process.env.HOST)
+  (!process.env.APP_URL ||
+    process.env.APP_URL === process.env.HOST)
 ) {
-  process.env.SHOPIFY_APP_URL = process.env.HOST;
+  process.env.APP_URL = process.env.HOST;
   delete process.env.HOST;
 }
 
-const host = new URL(process.env.SHOPIFY_APP_URL || "http://localhost")
+const host = new URL(process.env.APP_URL || "http://localhost")
   .hostname;
 
 let hmrConfig;
