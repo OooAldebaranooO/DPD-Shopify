a945f29fcf0514445e7f0b7b40b68872bb4a97e8
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-20 09:35:08 +0200

    Remove dpd-print-index extension

--- extensions/dpd-print-index/shopify.d.ts
+++
@@ -1,7 +0,0 @@
-import '@shopify/ui-extensions';
-
-//@ts-ignore
-declare module './src/PrintActionExtension.tsx' {
-  const shopify: import('@shopify/ui-extensions/admin.order-index.selection-print-action.render').Api;
-  const globalThis: { shopify: typeof shopify };
-}
