5585a19fc72ee723356fecdd0a70410f7be4758c
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-29 11:11:48 +0200

    Modifs page Appli

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -1,6 +1,7 @@
 import type { LoaderFunctionArgs, HeadersFunction } from "react-router";
 import { authenticate } from "../shopify.server";
 import { boundary } from "@shopify/shopify-app-react-router/server";
+import { useRouteError } from "react-router";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
   await authenticate.admin(request);
@@ -10,10 +11,14 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
 export default function Index() {
   return (
     <div style={{ padding: "20px" }}>
-      <h1>Impression DPD LiveDeco</h1>
-      <p>Test</p>
+      <h1>Test</h1>
     </div>
   );
+}
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return boundary.error(error);
 }
 
 export const headers: HeadersFunction = (headersArgs) => {
