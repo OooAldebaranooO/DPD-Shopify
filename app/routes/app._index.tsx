433519e87b02b7f9d0f99c21d3c57b9ba58a5425
OooAldebaranooO <ifttt.jo@gmail.com>
2026-05-29 12:44:14 +0200

    Modifs page Appli

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -1,17 +1,14 @@
 import type { LoaderFunctionArgs, HeadersFunction } from "react-router";
-import { authenticate } from "../shopify.server";
 import { boundary } from "@shopify/shopify-app-react-router/server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
-  await authenticate.admin(request);
   return null;
 };
 
 export default function Index() {
   return (
-    <div style={{ padding: "20px", fontFamily: "Arial" }}>
-      <h1>Impression DPD LiveDeco</h1>
-      <p>Page chargée ✅</p>
+    <div style={{ padding: "20px" }}>
+      <h1>Test</h1>
     </div>
   );
 }
